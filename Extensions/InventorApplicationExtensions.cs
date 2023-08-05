d476dc331f21e4b172c95d3e31ab81736c8540ca
Bret Leasure <bret.leasure@outlook.com>
2023-08-04 19:51:12 -0500

    changed namespace to Inventor for InventorApplicationExtensions

--- Extensions/InventorApplicationExtensions.cs
+++ Extensions/InventorApplicationExtensions.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Inventor;
-
-namespace ViewReference.Extensions
+﻿namespace Inventor
 {
     public static class InventorApplicationExtensions
     {
