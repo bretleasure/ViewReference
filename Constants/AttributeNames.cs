628d80458d3930d8ca2bb38dac034bf8958eadf5
Bret Leasure <bret.leasure@outlook.com>
2024-11-05 08:18:29 -0600

    Restructured Repo folders; Updated to dual target .NET472 and .NET8; Updated Inventor interop to Inventor 2023

---
+++ Constants/AttributeNames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewReference
+{
+    internal class AttributeNames
+    {
+        public const string ViewName = "ViewName";
+        public const string ViewSheetNumber = "ViewSheetNumber";
+        public const string ViewSheetName = "ViewSheetName";
+        public const string ParentSheetNumber = "ParentSheetNumber";
+        public const string ParentSheetName = "ParentSheetName";
+        public const string CalloutStyle = "CalloutStyle";
+        public const string ViewLabelStyle = "ViewLabelStyle";
+        public const string LabelText = "LabelText";
+    }
+}
---
+++ Settings/ViewReferenceAddinSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewReference
+{
+    internal class ViewReferenceAddinSettings
+    {
+        public bool UpdateBeforeSave { get; set; }
+        public ViewReferenceSettings ViewReferenceSettings { get; set; }
+    }
+}
